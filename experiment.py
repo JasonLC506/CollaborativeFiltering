2edefd32014f9df0871daf087e82bf14ff2b47ee
JasonLC506 <jpz5181@ist.psu.edu>
2017-08-03 17:50:40 -0400

    CDMultiClass.py similar problem with TDMultiClass, while when fixing rating latent vectors to be consistent with MFMultiClass, same results obtained

--- experiment.py
+++ experiment.py
@@ -1,18 +1,20 @@
 from MFMultiClass import MF
 from TDMultiClass import TD
+from CDMultiClass import CD
 from trainingDataGenerator import datagenerator
 
-def experiment(data_train, data_valid, data_test, ku, kv, kr, max_epoch = 10, SGDstep = 0.001):
-# def experiment(data_train, data_valid, data_test, K, max_epoch=10, SGDstep=0.001):
+# def experiment(data_train, data_valid, data_test, ku, kv, kr, max_epoch = 10, SGDstep = 0.001):
+def experiment(data_train, data_valid, data_test, K, max_epoch=10, SGDstep=0.001):
 
     training = datagenerator(data_train)
     valid = datagenerator(data_valid)
     test = datagenerator(data_test)
 
     # model = MF()
-    # model.fit(training, valid, K = K, max_epoch = max_epoch, SGDstep = SGDstep)
-    model = TD()
-    model.fit(training, valid, ku, kv, kr, max_epoch=max_epoch, SGDstep=SGDstep)
+    model = CD()
+    model.fit(training, valid, K = K, max_epoch = max_epoch, SGDstep = SGDstep)
+    # model = TD()
+    # model.fit(training, valid, ku, kv, kr, max_epoch=max_epoch, SGDstep=SGDstep)
     perf = performance(test, model)
     return perf
 
@@ -35,6 +37,6 @@ if __name__ == "__main__":
     print experiment(data_train = "data/synthetic_N500_M500_L3_K5_0.7train",
                      data_valid = "data/synthetic_N500_M500_L3_K5_0.1valid",
                      data_test = "data/synthetic_N500_M500_L3_K5_0.2test",
-                     ku = 15, kv = 15, kr = 2,
-                     # K = 5,
+                     # ku = 15, kv = 15, kr = 2,
+                     K = 15,
                      max_epoch = 1000, SGDstep = 0.001)
--- test.py
+++ test.py
@@ -6,6 +6,7 @@ import numpy as np
 from datetime import datetime
 from datetime import timedelta
 
-a = np.arange(2)
-b = np.arange(2)*10
-print np.dot(a,b)
+a = (np.arange(4)+1).reshape([2,2])
+b = np.arange(2)*10+1
+print a
+print np.multiply(a,b)
